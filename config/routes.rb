34c5663285e626ea5fbf79318bea1a91313b3de4
john <john@johnmayou.com>
2023-04-09 12:25:10 -0500

    destroy action created, refactored with before_action and used 'resources' for blog_posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  
+  resources :blog_posts
+
+  ## resources creates index
+  # get "/blog_posts", to: "blog_posts#index", as: :blog_posts
+  
+  ## 6 other routes that are associated with basic CRUD
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
